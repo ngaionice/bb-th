0b2a2dfc4261b7a6bd56b5c9eea0da9bdd28d1ca
ngaionice <julian.myne@gmail.com>
2022-02-15 11:36:04 -0500

    Update subscribe display text on CartScreen

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -35,7 +35,9 @@ function CartItem({ data, id }) {
       <Grid item xs={6} sm={7} lg={8}>
         <Stack>
           <Typography variant="body1">{name}</Typography>
-          <Typography variant="body2">{subscribe}</Typography>
+          <Typography variant="body2">
+            {subscribe === "One-time" ? "One-time" : "Monthly"}
+          </Typography>
         </Stack>
       </Grid>
       <Grid item xs={6} sm={5} lg={4} container alignItems="center">
